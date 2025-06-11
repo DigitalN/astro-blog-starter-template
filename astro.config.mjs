f4a548646265c9e1f75ecf1beae8eb7a11b453e1
DigitalN <keithm@outlook.com>
2025-06-11 16:36:10 -0600

    nexy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  // Cloudflare Workers adapter
-  adapter: cloudflare(),
-
-  // Other plugins / integrations
-  integrations: [tailwind()]
+  integrations: [tailwind()],   // static output by default
 });
